46a1640d792d17733c5fab575b2ef0f95f09f59d
bjornregnell <bjorn.regnell@cs.lth.se>
2024-09-13 16:32:24 +0200

    add more to comments on warning and classifier

--- project.scala
+++ project.scala
@@ -12,12 +12,14 @@
 // See lates JavaFX version here:  https://mvnrepository.com/artifact/org.openjfx/
 
 // You can ignore the warning "Unsupported JavaFX configuration: classes were loaded from unnamed module ..."
-// More details here: https://stackoverflow.com/questions/67854139
+/* More details on the warning here: 
+    https://stackoverflow.com/questions/67854139
+    https://github.com/scala/scala-dev/issues/529
 
-/* If the app does not start you may need to add a classifier in each using-clauses above 
+  If the app does not start you may need to add a classifier in each using-clauses above 
     such as classifier=win or classifier=mac or classifier=linux like so:
       //> using dep "org.openjfx:javafx-base:22.0.2,classifier=win"
     See artifact classifier for your OS here: 
       https://repo1.maven.org/maven2/org/openjfx/javafx-base/22.0.2/
-    If you are on arm then you need classifier=mac-aarch64 or classifier=linux-aarch64
+    If you are on ARM then you need classifier=mac-aarch64 or classifier=linux-aarch64
 */
